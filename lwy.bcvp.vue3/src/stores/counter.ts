925eb3a13cbebc71dca3116b987c514fd32e2b4b
lanweiye1 <lanweiye1@longcheer.com>
2025-04-02 16:08:30 +0800

    为计数器添加递减功能，并在HomeView中实现增减按钮和显示计数器状态

--- lwy.bcvp.vue3/src/stores/counter.ts
+++ lwy.bcvp.vue3/src/stores/counter.ts
@@ -7,6 +7,51 @@ export const useCounterStore = defineStore('counter', () => {
   function increment() {
     count.value++
   }
+  function decrement() {
+    count.value--
+  }
 
-  return { count, doubleCount, increment }
+  return { count, doubleCount, increment, decrement }
 })
+
+// Vuex类似写法  选项式API
+// export const useCounterStore = defineStore('counter', {
+//   // 定义状态
+//   state: () => ({
+//     count: 0,
+//   }),
+
+//   // 定义 actions，可以包含异步操作
+//   actions: {
+//     increment() {
+//       this.count++
+//     },
+//     decrement() {
+//       this.count--
+//     },
+//   },
+
+//   // 定义 getters，用来计算衍生的状态
+//   getters: {
+//     doubleCount: (state) => state.count * 2,
+//   },
+// })
+
+// Pinia 选项式 API
+// export const useCounterStore = defineStore('counter', {
+//   state: () => ({
+//     count: 0,
+//   }),
+//   getters: {
+//     doubleCount: (state) => state.count * 2,
+//   },
+//   actions: {
+//     increment() {
+//       this.count++
+//     },
+//     decrement() {
+//       this.count--
+//     },
+//   },
+//   // 不需要 return
+// })
--- lwy.bcvp.vue3/src/views/HomeView.vue
+++ lwy.bcvp.vue3/src/views/HomeView.vue
@@ -1,9 +1,49 @@
+<!-- 组合式API setup语法糖 -->
+
 <script setup lang="ts">
+import { computed } from 'vue'
 import TheWelcome from '../components/TheWelcome.vue'
+import { useCounterStore } from '@/stores/counter'
+
+// 获取store
+const counterStore = useCounterStore()
+
+//使用store中的状态和方法，不适用解构来保持响应特性
+const count = computed(() => counterStore.count)
+const doubleCount = computed(() => counterStore.doubleCount)
+const increment = counterStore.increment
+const decrement = counterStore.decrement
 </script>
+
+<!-- 组合式API -->
+<!-- <script>
+import { ref } from 'vue'
+
+export default {
+  setup() {
+    const count = ref(0)
+    const increment = () => {
+      count.value++
+    }
+    const decrement = () => {
+      count.value--
+    }
+    return {
+      count,
+      increment,
+      decrement
+    }
+    computed()
+  }
+}
+</script> -->
 
 <template>
   <main>
     <TheWelcome />
+    <p>Count: {{ count }}</p>
+    <p>DoubleCount: {{ doubleCount }}</p>
+    <button @click="increment">Increment</button>
+    <button @click="decrement">decrement</button>
   </main>
 </template>
